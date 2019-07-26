c5ddad20f1df8fdb90c671c1932865dfb0535f50
Brown <teb3@NIST.GOV>
2019-07-26 17:08:53 -0600

    Fixed sort from the surface using .iloc rather than descending Q values (not always the case)

--- AmpFitting.py
+++ AmpFitting.py
@@ -19,49 +19,54 @@ import glob
 def SHO(f, fo, Adrive, Q):
     return fo**2*Adrive/np.sqrt((fo**2-f**2)**2+(fo*f/Q)**2)
 
-files=glob.glob('*.csv')
+files=glob.glob('./AggregatedViscData/*/*NN8*.csv')
 AllCoeffs=[1]*len(files)
 for filenumber in range(len(files)):
     alldata = pd.read_csv(files[filenumber])
-    minFreq = 35000
-    maxFreq = 90000
+    minFreq = 45000
+    maxFreq = 65000
     alldata = alldata[alldata.iloc[:, 0] >= minFreq]
     alldata = alldata[alldata.iloc[:, 0] <= maxFreq]
     FreqData = alldata.iloc[:, 0]
-    AmpData = alldata.filter(like='Amp', axis = 1)
+    AmpData = alldata.filter(like='AmpV', axis = 1)
     plt.semilogy(FreqData, AmpData)
     
     coeffs=pd.DataFrame() 
-    
+    pnext = [55000, 0.005, 10]
     for col in range(len(AmpData.columns)):
         ydata = AmpData.values[:, col]
-        popt, pcov = curve_fit(SHO, FreqData, ydata, p0=[60000, .005, 5])
+        popt, pcov = curve_fit(SHO, FreqData, ydata, p0=pnext)
         coeffs=pd.concat([coeffs, pd.DataFrame(popt)], axis=1)
         plt.semilogy(FreqData, SHO(FreqData, *popt),':')
+        pnext = popt #reuse the fitted parms as the initial guess for the next curve
         
         
-    plt.xlabel('Frequency')
-    plt.ylabel('Amplitude')
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude [V - arb.]')
     plt.title(files[filenumber])
     plt.show()
+    
     
     coeffs.index=['fo','Adrive','Q']
     coeffs.columns = list(AmpData.columns)
     AllCoeffs[filenumber] = coeffs
-
+#plt.show()
+  
+allQ = pd.DataFrame()    
+    
 for filenumber in range(len(files)):
     a=AllCoeffs[filenumber].T
-    Qs=a.Q[a.Q<100].sort_values(ascending=False) # Remove the free resonance peak and sort from the droplet surface
-            
+    Qs=a.Q[a.Q<100].iloc[::-1] # Remove the free resonance trace (Q>100) and flip to sort from the droplet surface
+    allQ = pd.concat([allQ,Qs], axis = 1)        
     d = list(range(100,100*len(Qs)+100, 100)) # for 100 nm steps and starting point
         
-    k = 2.15 # N/m
-    Amp = 5 # nm
-    print(files[filenumber], '; peak force at 500 nm = ', k/Qs[4]*Amp, ' nN')    
-    print(files[filenumber], '; peak force at 1000 nm = ', k/Qs[9]*Amp, ' nN')  
-    plt.plot(d,1/Qs)
+#    k = 2.34 # N/m, spring constant
+#    Amp = 5 # nm, this amplitude can be changed to calculate the drag force at any amplitude in the linear range
+#    print(files[filenumber], '; peak force at 600 nm =', k/Qs[5]*Amp, 'nN')    
+#    print(files[filenumber], '; peak force at 1000 nm =', k/Qs[11]*Amp, 'nN')  
+    plt.plot(d,np.abs(1/Qs),'o')
     plt.xlabel('Insertion Length [nm]')
     plt.ylabel('1/Q')
-    plt.legend(list(files))
-    
+#    plt.legend(list(files))
+#plt.savefig('NN_N10.png',dpi=300)
 
